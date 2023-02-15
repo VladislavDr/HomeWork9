01d3988b25f78c4a33f6419a58a3de784854106f
Vladislad Dr <despod96@mail.ru>
2023-02-15 13:41:45 +0300

    task 1-3

---
+++ HomeWork2/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine ("Введите трехзначное число");
+string answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+int sumA = 0;
+int sum = 0;
+
+if(number > 99 && number < 10000) {
+    sumA = number / 10;
+    sum = sumA % 10;
+    Console.WriteLine(sum);
+}
---
+++ HomeWork2/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+//(номер цифры считается от левого края)
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6 
+
+Console.WriteLine("Введите целое число");
+string answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+int result = 0;
+
+if (number < 100){
+    Console.WriteLine("Третьей цифры нет");
+}
+
+while (number >= 1000){
+        number = number / 10;
+    }
+    result = number % 10;
+Console.WriteLine(result);
